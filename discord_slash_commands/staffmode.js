bf7afd28fbfa62b2c37da85d061ae0534a1902ca
QimieGames <45062664+QimieGames@users.noreply.github.com>
2022-09-26 20:43:54 +0800

    Update staffmode.js

--- discord_slash_commands/staffmode.js
+++ discord_slash_commands/staffmode.js
@@ -1,27 +1,71 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('staffmode')
-		.setDescription('EDIT LATER.')
-		.setDMPermission(false),
-	async execute(discordInteractionResult, discordInteractionDetails) {
+    data: new SlashCommandBuilder()
+        .setName('staffmode')
+        .setDescription('Toggle Staffmode Ingame. [Trusted+ User Command]')
+        .setDMPermission(false),
+    async execute(discordEmbedDetails, discordInteractionResultDetails, discordInteractionDetails, configValue, staffBot) {
 
-		discordInteractionResult.interactionFullCommand = `/${discordInteractionDetails.commandName}`;
+        discordInteractionResultDetails.interactionFullCommand = `/${discordInteractionDetails.commandName}`;
 
-		try {
-			await discordInteractionDetails.editReply({ content: 'TEST', ephemeral: false }).then(() => {
+        try {
 
-				discordInteractionResult.interactionResult = true;
+            const discordSlashCommandWhitelistedRolesID = [configValue.role_id.bot_admin, configValue.role_id.bot_trusted];
 
-			});
-		} catch {
-			await discordInteractionDetails.editReply({ content: '```Error occured while executing this command!```', ephemeral: false }).then(() => {
+            if (discordInteractionDetails.member.roles.cache.some(discordUserRoles => discordSlashCommandWhitelistedRolesID.includes(discordUserRoles.id)) !== true) {
+                await discordInteractionDetails.editReply({ content: '```You are not allowed to run this command!```', ephemeral: false }).then(() => {
 
-				discordInteractionResult.interactionResult = 'ERROR';
+                    discordInteractionResultDetails.interactionResult = false, discordInteractionResultDetails.interactionFailedReason = 'User has insufficient permission!';
 
-			});
-		}
-		return discordInteractionResult;
-	}
+                });
+            } else {
+
+                const staffmodeToggledMessageRegex = RegExp(/^STAFF \» Successfully (enabled|disabled) staff mode\!/);
+
+                const toggleStaffmode = staffBot.findMessage(10000, staffmodeToggledMessageRegex).then(async (toggleStaffmodeResult) => {
+                    if (toggleStaffmodeResult === false) {
+                        await discordInteractionDetails.editReply({ content: '```Error occured while toggling staffmode!```', ephemeral: false }).then(() => {
+
+                            discordInteractionResultDetails.interactionResult = false, discordInteractionResultDetails.interactionFailedReason = 'Error occured while toggling staffmode!';
+
+                        });
+                    } else {
+
+                        const staffBotUsername = staffBot.username;
+
+                        const staffmodeToggledDetails = String(toggleStaffmodeResult).match(staffmodeToggledMessageRegex);
+
+                        const staffmodeStatus = staffmodeToggledDetails[1].toUpperCase();
+
+                        const staffmodeToggledEmbedDescription = `IGN: ${staffBotUsername}\n` + `STATUS: ${staffmodeStatus}`;
+
+                        const staffmodeToggledEmbed = new EmbedBuilder()
+                            .setTitle('STAFFMODE TOGGLED')
+                            .setColor(discordEmbedDetails.color)
+                            .setThumbnail(discordEmbedDetails.thumbnail)
+                            .setDescription(staffmodeToggledEmbedDescription)
+                            .setFooter(discordEmbedDetails.footer)
+                            .setTimestamp();
+
+                        await discordInteractionDetails.editReply({ embeds: [staffmodeToggledEmbed], ephemeral: false }).then(() => {
+
+                            discordInteractionResultDetails.interactionResult = true;
+
+                        });
+                    }
+                });
+
+                staffBot.chat('/staffmode');
+                await toggleStaffmode;
+            }
+        } catch {
+            await discordInteractionDetails.editReply({ content: '```Error occured while executing this command!```', ephemeral: false }).then(() => {
+
+                discordInteractionResultDetails.interactionResult = 'ERROR';
+
+            });
+        }
+        return discordInteractionResultDetails;
+    }
 };
